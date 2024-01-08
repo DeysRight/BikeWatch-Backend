3cf8d7fdb9b1ff3c6d17c746eb68edbdb3eab148
ktpark <qkrrudxkr77@gmail.com>
2024-01-08 16:21:35 +0900

    git init

---
+++ src/main/java/com/bikeWatch/BikeWatchApplication.java
@@ -0,0 +1,12 @@
+package com.bikeWatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BikeWatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BikeWatchApplication.class, args);
+	}
+}
