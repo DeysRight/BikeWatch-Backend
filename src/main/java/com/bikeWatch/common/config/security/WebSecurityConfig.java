fd70890b3dd90775fc044827f75183cc67a9550b
ktpark <qkrrudxkr77@gmail.com>
2024-02-14 13:08:19 +0900

    fix: CORS 설정 오류

--- src/main/java/com/bikeWatch/common/config/security/WebSecurityConfig.java
+++ src/main/java/com/bikeWatch/common/config/security/WebSecurityConfig.java
@@ -43,7 +43,7 @@ public class WebSecurityConfig {
 	@Bean
 	public CorsConfigurationSource corsConfigurationSource() {
 		CorsConfiguration configuration = new CorsConfiguration();
-		configuration.addAllowedOrigin("*");
+		configuration.addAllowedOriginPattern("*");
 		configuration.addAllowedHeader("*");
 		configuration.addAllowedMethod("*");
 		configuration.setAllowCredentials(true);
