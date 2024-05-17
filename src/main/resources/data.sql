8d4f0281892f889db5803c778840c99bdba425a1
ktpark <qkrrudxkr77@gmail.com>
2024-05-17 17:55:58 +0900

    fix: 번호 수정

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,3 @@
 INSERT INTO information(seoul_price, gyeonggi_price, etc_price, phone_number)
-VALUES (40000, 60000, 20000, '010-4656-5799');
+VALUES (40000, 60000, 20000, '010-4656-5709');
        
